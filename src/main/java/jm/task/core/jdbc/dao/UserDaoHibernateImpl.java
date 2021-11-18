d725ac563a23206e302791fcd63e8e08de3f6e9b
AntonIzosimov <90404390+AntonIzosimov@users.noreply.github.com>
2021-11-19 01:46:10 +0300

    задача 1.1.4

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,8 +1,12 @@
 package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.SessionFactory;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+
+    SessionFactory sessionFactory = Util.getSessionFactory();
 
     public UserDaoHibernateImpl() {
 
@@ -11,29 +15,67 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
+        String sql = """
+                CREATE TABLE IF NOT EXISTS users(
+                id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                name TEXT,
+                lastName TEXT,
+                age TINYINT);
+                """;
+
+        try (var session = sessionFactory.openSession()) {
+            var transaction = session.beginTransaction();
+            session.createSQLQuery(sql).executeUpdate();
+            transaction.commit();
+        }
     }
 
     @Override
     public void dropUsersTable() {
+        String sql = "DROP TABLE IF EXISTS users";
 
+        try (var session = sessionFactory.openSession()) {
+            var transaction = session.beginTransaction();
+            session.createSQLQuery(sql).executeUpdate();
+            transaction.commit();
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
+        try (var session = sessionFactory.openSession()) {
+            var transaction = session.beginTransaction();
+            session.save(new User(name, lastName, age));
+            transaction.commit();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
+        try (var session = sessionFactory.openSession()) {
+            var transaction = session.beginTransaction();
+            User user = session.load(User.class, id);
+            session.delete(user);
+            transaction.commit();
+        }
 
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        try (var session = sessionFactory.openSession()) {
+            return (List<User>)session.createQuery("FROM User").list();
+        }
     }
 
     @Override
     public void cleanUsersTable() {
+        String sql = "TRUNCATE TABLE users";
 
+        try (var session = sessionFactory.openSession()) {
+            var transaction = session.beginTransaction();
+            session.createSQLQuery(sql).executeUpdate();
+            transaction.commit();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,8 +1,12 @@
 package jm.task.core.jdbc.util;
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
+import java.util.Properties;
 
 public final class Util {
     private static final String URL_KEY = "jdbc:mysql://localhost:3306/kata";
@@ -18,5 +22,18 @@ public final class Util {
         } catch (SQLException throwables) {
             throw new RuntimeException(throwables);
         }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        Properties properties = new Properties();
+        Configuration configuration = new Configuration();
+
+        properties.setProperty(Environment.URL, URL_KEY);
+        properties.setProperty(Environment.USER, USERNAME_KEY);
+        properties.setProperty(Environment.PASS, PASSWORD_KEY);
+        properties.setProperty(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
+
+        configuration.setProperties(properties).addAnnotatedClass(User.class);
+        return configuration.buildSessionFactory();
     }
 }
